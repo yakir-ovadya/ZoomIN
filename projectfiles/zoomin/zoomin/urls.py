c2f588facd1e417836bf32b35059e8e6b791edd1
HaielDahan <haielhd@ac.sce.ac.il>
2020-12-13 21:29:07 +0200

    חיאל - בוצע כפתור שחזור שם משתמש ללא גישה עדין לDB

--- projectfiles/zoomin/zoomin/urls.py
+++ projectfiles/zoomin/zoomin/urls.py
@@ -23,5 +23,4 @@ urlpatterns = [
     path('account/', include('account.urls')),
     path('account/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('schedule/', TemplateView.as_view(template_name='schedule.html'), name='schedule'),
 ]
