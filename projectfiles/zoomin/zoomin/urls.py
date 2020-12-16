e84efa18754e14be65597f9854bf1a1e80202215
elishevagevirtz <gevirel@ac.sce.ac.il>
2020-12-16 15:04:08 +0200

    temaplates, models
    temaplates, models

--- projectfiles/zoomin/zoomin/urls.py
+++ projectfiles/zoomin/zoomin/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('account/', include('account.urls')),
     path('account/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('schedule/', TemplateView.as_view(template_name='schedule.html'), name='schedule'),
+    path('grades/',include('account.urls')),
+
 ]
