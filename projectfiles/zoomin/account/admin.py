ba3caf1385e9c4269bbcaf7409fd2d0aa421e235
HaielDahan <haielhd@ac.sce.ac.il>
2020-12-20 13:41:56 +0200

    בוצע הוספת כפתור של שחזור שם משתמש- חיאל
    בוצע הוספת כפתור של שחזור שם משתמש- חיאל

--- projectfiles/zoomin/account/admin.py
+++ projectfiles/zoomin/account/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from account.models import Administrator,Teacher,Student,Parent,Course
 # Register your models here.
+
+
+admin.site.register(Administrator)
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(Parent)
+admin.site.register(Course)
--- projectfiles/zoomin/zoomin/urls.py
+++ projectfiles/zoomin/zoomin/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('account/', include('account.urls')),
     path('account/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('grades/',include('account.urls')),
+    path('UsernameRecovery/', TemplateView.as_view(template_name='UsernameRecovery.html'), name='UsernameRecovery'),
 ]
