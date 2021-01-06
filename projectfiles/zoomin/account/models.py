fd81b6ce70e187418c5e364dfac5581fd9df18a6
Nadav Ishai <nadavis@ac.sce.ac.il>
2021-01-06 20:44:22 +0200

    הוספת דף נוחכות, צפייה לתלמידים והורים, עריכה למנהל ומורה
    הוספת דף נוחכות, צפייה לתלמידים והורים, עריכה למנהל ומורה

--- projectfiles/zoomin/account/models.py
+++ projectfiles/zoomin/account/models.py
@@ -4,22 +4,27 @@ from datetime import date
 
 # Create your models here.
 class UserProfile(models.Model):
-    user = models.OneToOneField(User,null=True,blank=True, on_delete=models.CASCADE)
-    First_Name = models.CharField(max_length=30,null=True,blank=True)
-    Last_Name = models.CharField(max_length=30,null=True,blank=True)
-    Code = models.CharField(max_length=30,null=True,blank=True)
-    ID_Number = models.CharField(max_length=30,null=True,blank=True)
-
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True)
+    First_Name = models.CharField('שם פרטי', max_length=30, null=True, blank=True)
+    Last_Name = models.CharField('שם משפחה', max_length=30, null=True, blank=True)
+    Code = models.CharField('קוד הרשאה', max_length=30, null=True, blank=True)
+    ID_Number = models.CharField('תעודת זהות', max_length=30, null=True, blank=True)
+    in_class_op = (('נוכח', "נוכח"), ('לא נוכח', "לא נוכח"))
+    in_class = models.CharField('סטטוס', choices=in_class_op, default='לא נוכח', max_length=300, null=True)
+    class Meta:
+        verbose_name_plural = "userprofile"
 
     def __str__(self):
         return str(self.user)
 
 
+
 class board_school(models.Model):
     id = models.AutoField(primary_key=True)
-    topic = models.CharField('topic',max_length=100, blank=False, null=False)
-    description = models.TextField('Description', max_length=300, blank=True , null=True)
-    publication_date = models.DateField(default=date.today())
+    topic = models.CharField('נושא',max_length=100, blank=False, null=False)
+    description = models.TextField('תיאור', max_length=300, blank=True , null=True)
+    publication_date = models.DateField('תאריך',default=date.today())
 
     class Meta:
         verbose_name = 'Board_school'
@@ -33,9 +38,9 @@ class board_school(models.Model):
 
 class board_class(models.Model):
     id = models.AutoField(primary_key=True)
-    topic = models.CharField('topic',max_length=100, blank=False, null=False)
-    description = models.TextField('Description', max_length=300, blank=True , null=True)
-    publication_date = models.DateField(default=date.today())
+    topic = models.CharField('נושא',max_length=100, blank=False, null=False)
+    description = models.TextField('תיאור', max_length=300, blank=True , null=True)
+    publication_date = models.DateField('תאריך',default=date.today())
     class Meta:
         verbose_name = 'Board_class'
         verbose_name_plural = 'Board_class'
@@ -48,19 +53,19 @@ class board_class(models.Model):
 
 class schedule_mod(models.Model):
     id = models.AutoField(primary_key=True)
-    friday = models.CharField(max_length=100, blank=False, null=False)
-    friday_link = models.CharField(max_length=100, blank=False, null=False)
-    thursday = models.CharField(max_length=100, blank=False, null=False)
-    thursday_link = models.CharField(max_length=100, blank=False, null=False)
-    wednesday = models.CharField(max_length=100, blank=False, null=False)
-    wednesday_link = models.CharField(max_length=100, blank=False, null=False)
-    tuesday = models.CharField(max_length=100, blank=False, null=False)
-    tuesday_link = models.CharField(max_length=100, blank=False, null=False)
-    monday = models.CharField(max_length=100, blank=False, null=False)
-    monday_link = models.CharField(max_length=100, blank=False, null=False)
-    sunday = models.CharField(max_length=100, blank=False, null=False)
-    sunday_link = models.CharField(max_length=100, blank=False, null=False)
-    time = models.CharField(max_length=100, blank=False, null=False)
+    friday = models.CharField('יום שישי',max_length=100, blank=False, null=False)
+    friday_link = models.CharField('קישור-יום שישי',max_length=100, blank=False, null=False)
+    thursday = models.CharField('יום חמישי',max_length=100, blank=False, null=False)
+    thursday_link = models.CharField('קישור-יום חמישי',max_length=100, blank=False, null=False)
+    wednesday = models.CharField('יום רביעי',max_length=100, blank=False, null=False)
+    wednesday_link = models.CharField('קישור-יום רביעי',max_length=100, blank=False, null=False)
+    tuesday = models.CharField('יום שלישי',max_length=100, blank=False, null=False)
+    tuesday_link = models.CharField('קישור-יום שלישי',max_length=100, blank=False, null=False)
+    monday = models.CharField('יום שני',max_length=100, blank=False, null=False)
+    monday_link = models.CharField('קישור-יום שני',max_length=100, blank=False, null=False)
+    sunday = models.CharField('יום ראשון',max_length=100, blank=False, null=False)
+    sunday_link = models.CharField('קישור-יום ראשון',max_length=100, blank=False, null=False)
+    time = models.CharField('שעה',max_length=100, blank=False, null=False)
     topics = [friday, friday_link, thursday, thursday_link, wednesday, wednesday_link,
               tuesday, tuesday_link, monday, monday_link, sunday, sunday_link, time]
 
@@ -74,9 +79,9 @@ class schedule_mod(models.Model):
 class Test_Schedule(models.Model):
     id = models.AutoField(primary_key=True)
     profession = models.CharField('מקצוע',max_length=100, blank=False, null=False)
-    date = models.DateField(default=date.today(), blank=True , null=True)
-    start_time = models.TimeField(auto_now=False, auto_now_add=False, null=True)
-    end_time = models.TimeField(auto_now=False, auto_now_add=False, null=True)
+    date = models.DateField('תאאריך',default=date.today(), blank=True , null=True)
+    start_time = models.TimeField('שעת התחלה',auto_now=False, auto_now_add=False, null=True)
+    end_time = models.TimeField('שעת סיום',auto_now=False, auto_now_add=False, null=True)
     class Meta:
         verbose_name = 'Test_Schedule'
         verbose_name_plural = 'Test_Schedule'
@@ -85,3 +90,23 @@ class Test_Schedule(models.Model):
 
     def __str__(self):
         return self.profession
+
+
+class Presence_mod(models.Model):
+    id = models.AutoField(primary_key=True)
+    First_Name = models.CharField("שם פרטי", max_length=30, null=True, blank=True)
+    Last_Name = models.CharField("שם משפחה", max_length=30, null=True, blank=True)
+    exist = models.BooleanField("נוכח", default=False)
+    not_exist = models.BooleanField("לא נוכח", default=False)
+    date = models.DateField("תאריך", default=date.today(), blank=True , null=True)
+    profession = models.CharField("מקצוע", max_length=30, null=True, blank=True)
+    hour = models.TimeField("שעה", auto_now=False, auto_now_add=False, null=True)
+    val = [First_Name, Last_Name, exist, not_exist, date, profession, hour]
+
+    class Meta:
+        verbose_name = 'presence'
+        verbose_name_plural = 'presence'
+
+    def __str__(self):
+        return self.val
+
