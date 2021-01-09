645b9ff3861c3c30762747d1341cc00c799aae83
Nadav Ishai <nadavis@ac.sce.ac.il>
2021-01-09 20:23:38 +0200

    בוצעו 33 בדיקות יחידה  ל views, forms, models
    בוצעו 33 בדיקות יחידה  ל views, forms, models

--- projectfiles/zoomin/zoomin/settings.py
+++ projectfiles/zoomin/zoomin/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,8 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'account',
-
-
 
 ]
 
