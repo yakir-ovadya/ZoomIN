6a3eb952ab7c74a92435fae08931b0080f19f105
Yaki93 <yakirov@ac.sce.ac.il>
2021-01-09 19:06:07 +0200

    יקיר עובדיה - בוצע סיפור משתמש שחזור שם משתמש, ותיקון שגיאות זמן של דג'נגו
    יקיר עובדיה - בוצע סיפור משתמש שחזור שם משתמש, ותיקון שגיאות זמן של דג'נגו

--- projectfiles/zoomin/account/models.py
+++ projectfiles/zoomin/account/models.py
@@ -10,6 +10,17 @@ class UserProfile(models.Model):
     Last_Name = models.CharField('שם משפחה', max_length=30, null=True, blank=True)
     Code = models.CharField('קוד הרשאה', max_length=30, null=True, blank=True)
     ID_Number = models.CharField('תעודת זהות', max_length=30, null=True, blank=True)
+    in_class_op = (('נוכח', "נוכח"), ('לא נוכח', "לא נוכח"))
+    in_class = models.CharField('סטטוס', choices=in_class_op, default='לא נוכח', max_length=300, null=True)
+
+    pro1 = models.CharField('חשבון',max_length=30,default='לא ניתן ציון', null=True, blank=True)
+    pro2 = models.CharField('היסטוריה',max_length=30,default='לא ניתן ציון', null=True, blank=True)
+    pro3 = models.CharField('תנ"ך',max_length=30,default='לא ניתן ציון', null=True, blank=True)
+    pro4 = models.CharField('גאוגרפיה',max_length=30,default='לא ניתן ציון', null=True, blank=True)
+    pro5 = models.CharField('לשון',max_length=30,default='לא ניתן ציון', null=True, blank=True)
+    pro6 = models.CharField('ספרות',max_length=30,default='לא ניתן ציון', null=True, blank=True)
+    pro7 = models.CharField('ספורט',max_length=30,default='לא ניתן ציון', null=True, blank=True)
+
 
     class Meta:
         verbose_name_plural = "userprofile"
@@ -18,11 +29,12 @@ class UserProfile(models.Model):
         return str(self.user)
 
 
+
 class board_school(models.Model):
     id = models.AutoField(primary_key=True)
-    topic = models.CharField('topic',max_length=100, blank=False, null=False)
-    description = models.TextField('Description', max_length=300, blank=True , null=True)
-    publication_date = models.DateField(default=date.today())
+    topic = models.CharField('נושא',max_length=100, blank=False, null=False)
+    description = models.TextField('תיאור', max_length=300, blank=True , null=True)
+    publication_date = models.DateField('תאריך',default=str(date.today()))
 
     class Meta:
         verbose_name = 'Board_school'
@@ -36,9 +48,9 @@ class board_school(models.Model):
 
 class board_class(models.Model):
     id = models.AutoField(primary_key=True)
-    topic = models.CharField('topic',max_length=100, blank=False, null=False)
-    description = models.TextField('Description', max_length=300, blank=True , null=True)
-    publication_date = models.DateField(default=date.today())
+    topic = models.CharField('נושא',max_length=100, blank=False, null=False)
+    description = models.TextField('תיאור', max_length=300, blank=True , null=True)
+    publication_date = models.DateField('תאריך',default=str(date.today()))
     class Meta:
         verbose_name = 'Board_class'
         verbose_name_plural = 'Board_class'
@@ -51,19 +63,19 @@ class board_class(models.Model):
 
 class schedule_mod(models.Model):
     id = models.AutoField(primary_key=True)
-    friday = models.CharField(max_length=100, blank=False, null=False)
-    friday_link = models.CharField(max_length=100, blank=False, null=False)
-    thursday = models.CharField(max_length=100, blank=False, null=False)
-    thursday_link = models.CharField(max_length=100, blank=False, null=False)
-    wednesday = models.CharField(max_length=100, blank=False, null=False)
-    wednesday_link = models.CharField(max_length=100, blank=False, null=False)
-    tuesday = models.CharField(max_length=100, blank=False, null=False)
-    tuesday_link = models.CharField(max_length=100, blank=False, null=False)
-    monday = models.CharField(max_length=100, blank=False, null=False)
-    monday_link = models.CharField(max_length=100, blank=False, null=False)
-    sunday = models.CharField(max_length=100, blank=False, null=False)
-    sunday_link = models.CharField(max_length=100, blank=False, null=False)
-    time = models.CharField(max_length=100, blank=False, null=False)
+    friday = models.CharField('יום שישי',max_length=100, blank=False, null=False)
+    friday_link = models.CharField('קישור-יום שישי',max_length=100, blank=False, null=False)
+    thursday = models.CharField('יום חמישי',max_length=100, blank=False, null=False)
+    thursday_link = models.CharField('קישור-יום חמישי',max_length=100, blank=False, null=False)
+    wednesday = models.CharField('יום רביעי',max_length=100, blank=False, null=False)
+    wednesday_link = models.CharField('קישור-יום רביעי',max_length=100, blank=False, null=False)
+    tuesday = models.CharField('יום שלישי',max_length=100, blank=False, null=False)
+    tuesday_link = models.CharField('קישור-יום שלישי',max_length=100, blank=False, null=False)
+    monday = models.CharField('יום שני',max_length=100, blank=False, null=False)
+    monday_link = models.CharField('קישור-יום שני',max_length=100, blank=False, null=False)
+    sunday = models.CharField('יום ראשון',max_length=100, blank=False, null=False)
+    sunday_link = models.CharField('קישור-יום ראשון',max_length=100, blank=False, null=False)
+    time = models.CharField('שעה',max_length=100, blank=False, null=False)
     topics = [friday, friday_link, thursday, thursday_link, wednesday, wednesday_link,
               tuesday, tuesday_link, monday, monday_link, sunday, sunday_link, time]
 
@@ -77,9 +89,9 @@ class schedule_mod(models.Model):
 class Test_Schedule(models.Model):
     id = models.AutoField(primary_key=True)
     profession = models.CharField('מקצוע',max_length=100, blank=False, null=False)
-    date = models.DateField(default=date.today(), blank=True , null=True)
-    start_time = models.TimeField(auto_now=False, auto_now_add=False, null=True)
-    end_time = models.TimeField(auto_now=False, auto_now_add=False, null=True)
+    date = models.DateField('תאאריך',default=str(date.today()), blank=True , null=True)
+    start_time = models.CharField('שעת התחלה',max_length=30,default='09:00', null=True, blank=True)
+    end_time = models.CharField('שעת סיום',max_length=30,default='12:00', null=True, blank=True)
     class Meta:
         verbose_name = 'Test_Schedule'
         verbose_name_plural = 'Test_Schedule'
@@ -88,3 +100,23 @@ class Test_Schedule(models.Model):
 
     def __str__(self):
         return self.profession
+
+
+class Presence_mod(models.Model):
+    id = models.AutoField(primary_key=True)
+    First_Name = models.CharField("שם פרטי", max_length=30, null=True, blank=True)
+    Last_Name = models.CharField("שם משפחה", max_length=30, null=True, blank=True)
+    exist = models.BooleanField("נוכח", default=False)
+    not_exist = models.BooleanField("לא נוכח", default=False)
+    date = models.DateField("תאריך", default=str(date.today()), blank=True , null=True)
+    profession = models.CharField("מקצוע", max_length=30, null=True, blank=True)
+    hour = models.TimeField("שעה", auto_now=False, auto_now_add=False, null=True)
+    val = [First_Name, Last_Name, exist, not_exist, date, profession, hour]
+
+    class Meta:
+        verbose_name = 'presence'
+        verbose_name_plural = 'presence'
+
+    def __str__(self):
+        return self.val
+
--- projectfiles/zoomin/account/urls.py
+++ projectfiles/zoomin/account/urls.py
@@ -3,14 +3,20 @@ from django.urls import path
 
 from .views import schedule, register, grades, bulletin_board, bulletin_Schedule, presence, schedule, Username_Recovery, \
     bulletin_board_class, addBoardSchool, editBoardSchool, deleteBoardSchool,addBoardClass,editBoardClass,deleteBoardClass,\
-    addSchedule, editSchedule, deleteSchedule, addTest, deleteTest, editTest, SearchResultsView, editSearch, deleteSearch
+    addSchedule, editSchedule, deleteSchedule, addTest, deleteTest, editTest, SearchResultsView, editSearch, deleteSearch,\
+    addpresence, editpresence, showthis, editpre, show_grades, gradesedit,username_rec
 urlpatterns = [
     path('signup', register, name='signup'),
-    path('grades', grades, name='grades'),
+
     path('bulletinboard', bulletin_board, name='bulletin_board'),
     path('bulletin_Schedule', bulletin_Schedule, name='bulletin_Schedule'),
-    path('presence', presence, name='presence'),
+
+    path('presence/', showthis.as_view(), name='presence'),
+    path('presence/editpre/<int:id>', editpre, name='editpre'),
+    path('presence/addpresence', addpresence, name='addpresence'),
+
     path('usernamerecovery/', Username_Recovery, name='usernamerecovery'),
+
     path('schedule', schedule, name='schedule'),
     path('schedule/addschedule', addSchedule, name='addSchedule'),
     path('schedule/editschedule/<int:id>', editSchedule, name='editSchedule'),
@@ -30,6 +36,13 @@ urlpatterns = [
     path('search/', SearchResultsView.as_view(), name='search_results'),
     path('search/editSearch/<int:id>', editSearch, name='editSearch'),
     path('search/deleteSearch/<int:id>', deleteSearch, name='deleteSearch'),
+
+    path('grades', show_grades.as_view(), name='grades'),
+    path('grades/gradesedit/<int:id>', gradesedit , name='gradesedit'),
+
+    path('username_rec_res/', username_rec.as_view(), name='username_rec_res'),
+
+
 
 
 
